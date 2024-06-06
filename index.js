6324608d2908cfd3668035699c4b0cd1171d9f36
Levi-21545 <lgomes.lrg@gmail.com>
2024-06-06 19:23:18 -0300

    build: Iniciando o projeto
    Criação inicial de pastas em arquitetura MVC

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+// MODELS
+const Tought = require("./models/Tought");
+
+//IMPORT ROUTES
+const toughtsRoutes = require("./routes/toughtsRoutes");
+
+// IMPORT CONTROLLER
+const ToughtController = require("./controllers/ToughtController");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// RECEBER RESPOSTA DO BODY
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//SESSION MIDDLEWARE
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+// FLASH MESSAGES
+app.use(flash);
+
+//PUBLIC PATH
+app.use(express.static("public"));
+
+// SET SESSION TO RES
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
+// ROUTES
+app.use("/toughts", toughtsRoutes);
+
+app.get('/', ToughtController.showToughts)
+
+conn
+  .sync()
+  //.sync({ force: true })
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
